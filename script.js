160f50a4ddbd4d9b80ffd2f088912aded8bd3637
Reny Alfonzo <109688916+Reny-Alfonzo@users.noreply.github.com>
2024-02-29 17:34:16 -0500

    subiendo a la nube

---
+++ script.js
@@ -0,0 +1,32 @@
+function subir() {
+    // Obtenemos la URL del iframe
+    const urlIframe = "index.html";
+
+    // Redirigimos a la URL del iframe
+    window.location.href = urlIframe;
+}
+
+function KungFuPanda4() {
+    // Obtenemos la URL del iframe
+    const urlIframe = "https://player.sensacine.com/19574218.html";
+
+    // Redirigimos a la URL del iframe
+    window.location.href = urlIframe;
+}
+
+function Elreinodelplanetadelossimios() {
+    // Obtenemos la URL del iframe
+    const urlIframe = "https://player.sensacine.com/19574522.html";
+
+    // Redirigimos a la URL del iframe
+    window.location.href = urlIframe;
+}
+
+function Garfield() {
+    // Obtenemos la URL del iframe
+    const urlIframe = "https://player.sensacine.com/19574054.html";
+
+    // Redirigimos a la URL del iframe
+    window.location.href = urlIframe;
+}
+
