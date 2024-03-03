353bc7022eec18f7f7cb0738f56e5dcb14a6fe44
Reny Alfonzo <109688916+Reny-Alfonzo@users.noreply.github.com>
2024-03-03 16:42:38 -0500

    configurando enlaces

--- script.js
+++ script.js
@@ -1,32 +1,5 @@
+
 function subir() {
-    // Obtenemos la URL del iframe
     const urlIframe = "index.html";
-
-    // Redirigimos a la URL del iframe
-    window.location.href = urlIframe;
-}
-
-function KungFuPanda4() {
-    // Obtenemos la URL del iframe
-    const urlIframe = "https://www.youtube.com/embed/kg3Q63gzF6I?si=p2HuDYefCtCJG4XC";
-
-    // Redirigimos a la URL del iframe
-    window.location.href = urlIframe;
-}
-
-function Elreinodelplanetadelossimios() {
-    // Obtenemos la URL del iframe
-    const urlIframe = "https://www.youtube.com/embed/fWWrW_VLjws?si=yWPZDOR1bjFig8pA";
-
-    // Redirigimos a la URL del iframe
-    window.location.href = urlIframe;
-}
-
-function Garfield() {
-    // Obtenemos la URL del iframe
-    const urlIframe = "https://www.youtube.com/embed/GeR3YxTv_zU?si=4niRToZuImSywDb9";
-
-    // Redirigimos a la URL del iframe
-    window.location.href = urlIframe;
-}
-
+     window.location.href = urlIframe;
+ }
